75fd9ca31fbbfbca5781fd734f848426f4c15110
SDxBacon <z141134553@gmail.com>
2023-08-20 23:44:52 +0800

    feat: 🎸 finished first version without test cases

---
+++ src/components/App/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Flex,
+  Text,
+  Icon,
+  Menu,
+  MenuList,
+  MenuItem,
+  TonicProvider,
+  colorStyle,
+} from '@tonic-ui/react';
+// import components
+import AvatarMenuButton from '@components/AvatarMenuButton';
+// import .css
+import '@src/styles/global.css';
+
+/**
+ * App component - The entry component of this react project
+ */
+function App() {
+  return (
+    <TonicProvider
+      colorMode={{ defaultValue: 'dark' }}
+      colorStyle={{ defaultValue: colorStyle }}
+      useCSSBaseline // apply` CSS reset and base styles
+    >
+      <Flex height='inherit' alignItems='center' justifyContent='center'>
+        <Menu>
+          {/* MenuButton with Avatar */}
+          <AvatarMenuButton />
+          {/* MenuList - The layout of the menu is fixed. */}
+          <MenuList width='max-content'>
+            <MenuItem>
+              <Icon icon='settings' mr='2x' />
+              <Text>Settings</Text>
+            </MenuItem>
+            <MenuItem>
+              <Icon icon='user-team' mr='2x' />
+              <Text>Accounts</Text>
+            </MenuItem>
+            <MenuItem>
+              <Icon icon='lock' mr='2x' />
+              <Text>Privacy control</Text>
+            </MenuItem>
+          </MenuList>
+        </Menu>
+        {/* </DemoSection> */}
+      </Flex>
+    </TonicProvider>
+  );
+}
+
+export default App;
---
+++ src/components/AvatarMenuButton/AvatarMenuButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Flex,
+  FlexProps,
+  Icon,
+  MenuToggle,
+  useColorMode,
+  useColorStyle,
+} from '@tonic-ui/react';
+
+const Avatar = (props: FlexProps) => (
+  <Flex
+    p='1x'
+    borderRadius='50%'
+    alignItems='center'
+    justifyContent='center'
+    fontSize='xs'
+    lineHeight='1'
+    {...props}
+  />
+);
+
+const AvatarMenuButton = () => {
+  const [colorMode] = useColorMode();
+  const [colorStyle] = useColorStyle({ colorMode }) as any;
+  return (
+    <MenuToggle>
+      <Avatar
+        backgroundColor={colorStyle.background.secondary}
+        color={colorStyle.color.secondary}
+        _hover={{
+          color: colorStyle.color.primary,
+        }}
+      >
+        <Icon icon='user-team' size='5x' />
+      </Avatar>
+    </MenuToggle>
+  );
+};
+
+export default AvatarMenuButton;
